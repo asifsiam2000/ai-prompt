e30dea4428fee9870247afbbd83417e60b4c47a0
asifsiam2000 <asifahamedsheam00@gmail.com>
2026-04-29 21:12:00 +0600

    all section is done

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -1,15 +1,24 @@
+import Category from "@/components/Category";
 import PhotoCard from "@/components/PhotoCard";
 
-const AllPhotos = async () => {
+const AllPhotos = async ({ searchParams }) => {
+    const { category } = await searchParams;
+    console.log("category is ", category);
     const res = await fetch('https://ai-prompt-lemon-seven.vercel.app/data.json');
     const photos = await res.json();
-    console.log(photos);
+
+
+    const filteredPhotos = category ? photos.filter(photo => photo.category.toLowerCase() == category.toLowerCase()) : photos;
+
+
+    
     return (
         <div>
             <h1 className="text-2xl font-bold my-5">All Photos </h1>
+            <Category></Category>
             <div className="grid grid-cols-4 gap-5">
                 {
-                    photos.map(photo => <PhotoCard key={photo.id} photo={photo}></PhotoCard>)
+                    filteredPhotos.map(photo => <PhotoCard key={photo.id} photo={photo}></PhotoCard>)
                 }
             </div>
         </div>
---
+++ src/components/Category.jsx
@@ -0,0 +1,22 @@
+import { Button } from "@heroui/react";
+import Link from "next/link";
+
+
+const Category = async () => {
+    const res = await fetch('https://ai-prompt-lemon-seven.vercel.app/category.json');
+    const categorys = await res.json();
+    console.log("hima", categorys);
+    return (
+        <div className="my-5 space-x-1">
+            {
+                categorys.map(category =><Link key={category.id} href={`/all-photos?category=${category.name}`}>
+                 <Button variant="outline"  className="mr-1">{ category.name}</Button>
+                </Link>
+
+                )
+            }
+        </div>
+    );
+};
+
+export default Category;
